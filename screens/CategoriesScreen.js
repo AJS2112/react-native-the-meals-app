2987f0981336292b3d4cb4bd9581aab105261001
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-13 12:41:34 -0300

    navigating-between-screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const CategoriesScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Categories Screen!</Text>
+            <Button title="Go to Meals!" onPress={() => {
+                props.navigation.navigate('CategoryMeals');
+            }} />
         </View>
     )
 };
@@ -13,7 +16,8 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignContent: 'center'
+        alignContent: 'center',
+        //padding: 10
     }
 });
 
