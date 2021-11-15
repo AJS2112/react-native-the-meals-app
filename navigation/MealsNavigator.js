d5e515dccdfca20d3edbed4beaf2a851aa650dbf
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-15 07:21:42 -0300

    default-navigation-options-and-config

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,3 +1,6 @@
+import { Platform } from 'react-native';
+import Colors from '../constants/Colors';
+
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import CategoriesScreen from '../screens/CategoriesScreen';
@@ -8,6 +11,13 @@ const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen,
     CategoryMeals: { screen: CategoryMealsScreen },
     MealDetail: MealDetailScreen
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    }
 });
 
 export default createAppContainer(MealsNavigator);
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Platform } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
-import Colors from "../constants/Colors";
 
 const CategoryMealsScreen = props => {
     const catId = props.navigation.getParam('categoryId');
@@ -24,11 +23,7 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
     return {
-        headerTitle: selectedCategory.title,
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+        headerTitle: selectedCategory.title
     };
 };
 
