e4a862b28fdf2a5d97dc8cf79311ec1fdcf6a656
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-22 16:21:53 -0300

    rendering-fav-fallback-text

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { HeaderButton } from '../components/HeaderButton';
 import { useSelector } from 'react-redux';
@@ -7,6 +8,13 @@ import MealList from "../components/MealList";
 
 const FavoritesScreen = props => {
     const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+    if (favMeals.length === 0 || !favMeals) {
+        return (<View style={styles.content}>
+            <Text>No favorite meals found. Start adding some!</Text>
+        </View>)
+    }
+
     return (
         <MealList listData={favMeals} navigation={props.navigation} />
     )
@@ -21,6 +29,12 @@ FavoritesScreen.navigationOptions = navData => {
     }
 };
 
-
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default FavoritesScreen;
