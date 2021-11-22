cfe84ece3a70b4621e2ed4720c5c63f0613cd18c
Antonio Sucre <antonio.sucre@indikatore.com>
2021-11-22 16:17:02 -0300

    switching-favorites-icon

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -18,6 +18,7 @@ const ListItem = props => {
 const MealDetailScreen = props => {
     const availableMeals = useSelector(state => state.meals.meals);
     const mealId = props.navigation.getParam('mealId');
+    const currentMealIsFavorite = useSelector(state => state.meals.favoriteMeals.some(meal => meal.id === mealId));
 
     const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
@@ -32,6 +33,9 @@ const MealDetailScreen = props => {
         props.navigation.setParams({ toggleFav: toggleFavoriteHandler });
     }, [toggleFavoriteHandler]);
 
+    useEffect(() => {
+        props.navigation.setParams({ isFav: currentMealIsFavorite });
+    }, [currentMealIsFavorite]);
 
     return (
         <ScrollView>
@@ -60,11 +64,16 @@ MealDetailScreen.navigationOptions = navigationData => {
     const mealTitle = navigationData.navigation.getParam('mealTitle');
     //const selectedMeal = MEALS.find(meal => meal.id === mealId);
     const toggleFavorite = navigationData.navigation.getParam('toggleFav');
+    const isFavorite = navigationData.navigation.getParam('isFav');
 
     return {
         headerTitle: mealTitle,
         headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item title='Favorite' iconName='ios-star' onPress={toggleFavorite} />
+            <Item
+                title='Favorite'
+                iconName={isFavorite ? 'ios-star' : 'ios-star-outline'} //'ios-star'
+                onPress={toggleFavorite}
+            />
         </HeaderButtons>
     };
 };
